26897b6a061768ebbb87ceb11dc50eb4ef6c02b3
GeNicolas <nicolas.gercin@hotmail.fr>
2019-01-10 16:03:47 +0100

    modif .gitignore et implementation code pour hierbernate

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,9 +1,15 @@
 package App;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import util.Context;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Context.getInstance();
 
 	}
 
