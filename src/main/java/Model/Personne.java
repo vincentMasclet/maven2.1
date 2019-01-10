a781c05279af1b69fa5111821f3c5ed3335d79c1
GeNicolas <nicolas.gercin@hotmail.fr>
2019-01-10 17:16:43 +0100

    Implementation code jpa pour communiquer avec une DB

---
+++ src/main/java/Model/Personne.java
@@ -0,0 +1,81 @@
+package Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="person")
+@SequenceGenerator(name="seqPersonne", sequenceName = "seq_person", allocationSize = 1, initialValue =1)
+public class Personne {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="seqPersonne")
+	private Integer id;
+	@Column(name = "first_name", length =150,nullable=true)
+	private String nom;
+	@Column(name = "last_name", length =150,nullable=true)
+	private String prenom;
+	
+	
+	
+	
+	public Personne() {
+		super();
+	}
+
+	public Personne(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
